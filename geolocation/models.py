0e68971fadcc9655a3eae776c3b366d70ea0003f
karzio <karol0602@gmail.com>
2025-01-07 13:24:23 +0100

    Define GeoLocation model

--- geolocation/models.py
+++ geolocation/models.py
@@ -2,7 +2,22 @@ from django.db import models
 
 
 class GeoLocation(models.Model):
-    name = models.CharField(max_length=255)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    web_address = models.CharField(max_length=255)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    country = models.CharField(max_length=60, blank=True)
+    city = models.CharField(max_length=60, blank=True)
+    zip = models.CharField(max_length=10, blank=True)
+    web_address = models.CharField(
+        max_length=255,
+        unique=True,
+        help_text="Web address or IP address. "
+        "Start with a protocol (http:// or https://)",
+    )
+
+    def __str__(self):
+        return (
+            f"Host: {self.web_address} is from {self.country}, {self.city}, {self.zip}"
+        )
+
+    def __repr__(self):
+        return f"GeoLocation(web_address={self.web_address}, latitude={self.latitude}, longitude={self.longitude}, city='{self.city}', country='{self.country}')"
